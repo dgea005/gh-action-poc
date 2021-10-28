8f0dc171da9c5baae23f4388f712ece3c2e6dc64
dgea005 <danielgeals@gmail.com>
2021-10-29 00:08:23 +0200

    create the dir

--- my_script.py
+++ my_script.py
@@ -3,6 +3,7 @@ import pathlib
 
 def write_file_using_inputs():
     current_dir = pathlib.Path(__file__).parent
+    pathlib.Path(current_dir/"templates").mkdir(parents=True, exist_ok=True)
     with open(current_dir/"templates/my_file.txt", "w") as f:
         f.write("test")
 
