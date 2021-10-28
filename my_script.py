9d96474ec787bb161773d05365749b568fd8cc80
dgea005 <danielgeals@gmail.com>
2021-10-29 00:32:22 +0200

    try to push

--- my_script.py
+++ my_script.py
@@ -1,12 +1,14 @@
 ##
 import pathlib
 
-def write_file_using_inputs():
+def main():
     current_dir = pathlib.Path(__file__).parent
     pathlib.Path(current_dir/"templates").mkdir(parents=True, exist_ok=True)
+    with open(current_dir/"input/input.txt", "r") as f:
+        data = f.read()
     with open(current_dir/"templates/my_file.txt", "w") as f:
-        f.write("test")
+        f.write(data)
 
 
 if __name__ == "__main__":
-    write_file_using_inputs()
+    main()
